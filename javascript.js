10a1f849aaff386319198d19e7c24226737bf49e
conorjack <178677262+conorjack@users.noreply.github.com>
2024-09-21 13:54:41 -0500

    Determine winner and increment score

--- javascript.js
+++ javascript.js
@@ -41,23 +41,42 @@ let computerScore = 0;
 // Create function to play a single round
 // Compare computer and human choice, determine winner
 function determineWinner(computer, human) {
-    if (computer.toLowerCase() === "rock" && human === "scissors") {
-        return winner = computer
-    } else if (computer.toLowerCase() === "paper" && human === "rock") {
-        return winner = computer
-    } else if (computer.toLowerCase() === "scissors" && human === "paper") {
-        return winner = computer
-    } else if (human === "rock" && computer.toLowerCase() === "scissors") {
-        return winner = human
-    } else if (human === "paper" && computer.toLowerCase() === "rock") {
-        return winner = human
-    } else if (human === "scissors" && computer.toLowerCase() === "paper") {
-        return winner = human
+    if (computer === "rock" && human === "scissors") {
+        return winner = "computer"
+    } else if (computer === "paper" && human === "rock") {
+        return winner = "computer"
+    } else if (computer === "scissors" && human === "paper") {
+        return winner = "computer"
+    } else if (human === "rock" && computer === "scissors") {
+        return winner = "human"
+    } else if (human === "paper" && computer === "rock") {
+        return winner = "human"
+    } else if (human === "scissors" && computer === "paper") {
+        return winner = "human"
     } else {
-        return winner = 0;
+        return winner = "neither";
     }
 }
 determineWinner(computerSelection, humanSelection);
 console.log(winner);
+
+// Determine and increment score
+function determineScore() {
+    if (winner === "computer") {
+        console.log("Sorry, the computer won. Better luck next round...")
+        return computerScore++;
+    } else if (winner === "neither") {
+       console.log("Wow, looks like you tied this round!")
+    } else if (winner === "human") {
+        console.log("Congrats, you won this round!")
+        return humanScore++;
+    } 
+}
+
+determineScore();
+
+// Display current score
+console.log("Your Score: " + humanScore);
+console.log("Computer Score: " + computerScore);
 
 // Display winner
