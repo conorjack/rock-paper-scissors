574a17de0b2c02021d405e73d64703a7f825119b
conorjack <178677262+conorjack@users.noreply.github.com>
2024-09-21 13:03:50 -0500

    Clean up function call and add selection clarity
    I set the function call to variables to allow for comparison. I also
    added selection clarification for reference.

--- javascript.js
+++ javascript.js
@@ -1,52 +1,95 @@
-// Computer Choice
+// Computer Choice *************
 
 // Create array of three options
 const computerChoice = [
-    "Rock",
-    "Paper",
-    "Scissors"
+    "rock",
+    "paper",
+    "scissors"
 ];
 
 // Define function to randomly select one of these three from array position
 function getComputerChoice() {
-    return computerChoice[Math.floor(Math.random() * (2 - 0 + 1))];
+    return console.log("Computer Selection: " + computerChoice[Math.floor(Math.random() * (2 - 0 + 1))]);
 }
-
 // Computer returns randomly rock, paper, or scissors
-console.log(getComputerChoice()); 
-
-
-
-// Human choice
-
+// Human choice ***************
 function getHumanChoice() {
 // Get player input
     let humanChoice = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
     // Validate user input
-    while (['rock', 'paper', 'scissors'].indexOf(humanChoice) === -1) {
-        humanChoice = prompt("Please choose only Rock, Paper, or Scissors").toLowerCase();
+    while (['rock', 'paper', 'scissors'].indexOf(humanChoice.toLowerCase()) === -1) {
+        humanChoice = prompt("Please Choose Rock, Paper, or Scissors").toLowerCase();
     }
     // Display valid user input
-    console.log(humanChoice);
+    return console.log("Your Selection: " + humanChoice);
 }
 
-console.log(getHumanChoice());
 
-// Players score variables
+// Players score variables ******
 // Create variable to capture user score
 let humanScore = 0;
 // Create variable to capture computer score
 let computerScore = 0;
 
-// Play a single round
+// Play a single round **********
 // Create function to play a single round
 
-function playRound(humanChoice, computerChoice) {
-
-}
-
 // Define human choice
+let humanSelection = getHumanChoice();
 // Define computer choice
-// Compare the two choices and determine a winner
-// Display the round winner
-// Increment the score of the winner
+let computerSelection = getComputerChoice();
+// Compare the two choices
+
+// function compareSelection(human, computer) {
+// }
+// // Determine the winner
+
+// let winner = undefined;
+
+// function checkWinner(computer, human) {
+//     if (computer.toLowerCase() === "rock" && human === "scissors") {
+//         return winner = computer
+//     } else if (computer.toLowerCase() === "paper" && human === "rock") {
+//         return winner = computer
+//     } else if (computer.toLowerCase() === "scissors" && human === "paper") {
+//         return winner = computer
+//     } else if (human === "rock" && computer.toLowerCase() === "scissors") {
+//         return winner = human
+//     } else if (human === "paper" && computer.toLowerCase() === "rock") {
+//         return winner = human
+//     } else if (human === "scissors" && computer.toLowerCase() === "paper") {
+//         return winner = human
+//     } else {
+//         return winner = 0;
+//     }
+// }
+
+// checkWinner(computerSelection, humanSelection);
+
+// console.log(winner);
+
+// // // Display round winner message
+// // if (winner = computer) {
+// //     console.log("Sorry, ${computerSelection} beats ${humanSelection}!")
+// // } else if (winner = human) {
+// //     console.log("Congrats! ${humanSelection} beats ${computerSelection}!")
+// // } else {
+// //     console.log("Wow, it was a tie! You both selected ${humanSelection}.")
+// // }
+
+// // Increment the score of the winner
+// humanScore++;
+// computerScore++;
+
+// function playRound(humanChoice, computerChoice) {
+//     console.log(humanChoice);
+//     console.log(computerChoice);
+// }
+
+// playRound(humanSelection, computerSelection);
+
+
+
+
+// // Display the round winner
+// // Increment the score of the winner
