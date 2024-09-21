e5f9599f67a6f0b9c338102c65e14324c55ab036
conorjack <178677262+conorjack@users.noreply.github.com>
2024-09-21 13:32:56 -0500

    Create determineWinner function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 // Computer Choice *************
 
 // Create array of three options
-const computerChoice = [
+let computerChoice = [
     "rock",
     "paper",
     "scissors"
@@ -9,21 +9,24 @@ const computerChoice = [
 
 // Define function to randomly select one of these three from array position
 function getComputerChoice() {
-    return console.log("Computer Selection: " + computerChoice[Math.floor(Math.random() * (2 - 0 + 1))]);
+    computerSelection = computerChoice[Math.floor(Math.random() * (2 - 0 + 1))]
+    return console.log("Computer Selection: " + computerSelection);
 }
 // Computer returns randomly rock, paper, or scissors
 // Human choice ***************
+let humanSelection = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
 function getHumanChoice() {
 // Get player input
-    let humanChoice = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
     // Validate user input
-    while (['rock', 'paper', 'scissors'].indexOf(humanChoice.toLowerCase()) === -1) {
-        humanChoice = prompt("Please Choose Rock, Paper, or Scissors").toLowerCase();
+    while (['rock', 'paper', 'scissors'].indexOf(humanSelection.toLowerCase()) === -1) {
+        humanSelection = prompt("Please Choose Rock, Paper, or Scissors").toLowerCase();
     }
     // Display valid user input
-    return console.log("Your Selection: " + humanChoice);
+    return console.log("Your Selection: " + humanSelection);
 }
 
+getComputerChoice();
+getHumanChoice();
 
 // Players score variables ******
 // Create variable to capture user score
@@ -31,65 +34,30 @@ let humanScore = 0;
 // Create variable to capture computer score
 let computerScore = 0;
 
+// console.log(computerSelection);
+// console.log(humanSelection);
+
 // Play a single round **********
 // Create function to play a single round
+// Compare computer and human choice, determine winner
+function determineWinner(computer, human) {
+    if (computer.toLowerCase() === "rock" && human === "scissors") {
+        return winner = computer
+    } else if (computer.toLowerCase() === "paper" && human === "rock") {
+        return winner = computer
+    } else if (computer.toLowerCase() === "scissors" && human === "paper") {
+        return winner = computer
+    } else if (human === "rock" && computer.toLowerCase() === "scissors") {
+        return winner = human
+    } else if (human === "paper" && computer.toLowerCase() === "rock") {
+        return winner = human
+    } else if (human === "scissors" && computer.toLowerCase() === "paper") {
+        return winner = human
+    } else {
+        return winner = 0;
+    }
+}
+determineWinner(computerSelection, humanSelection);
+console.log(winner);
 
-// Define human choice
-let humanSelection = getHumanChoice();
-// Define computer choice
-let computerSelection = getComputerChoice();
-// Compare the two choices
-
-// function compareSelection(human, computer) {
-// }
-// // Determine the winner
-
-// let winner = undefined;
-
-// function checkWinner(computer, human) {
-//     if (computer.toLowerCase() === "rock" && human === "scissors") {
-//         return winner = computer
-//     } else if (computer.toLowerCase() === "paper" && human === "rock") {
-//         return winner = computer
-//     } else if (computer.toLowerCase() === "scissors" && human === "paper") {
-//         return winner = computer
-//     } else if (human === "rock" && computer.toLowerCase() === "scissors") {
-//         return winner = human
-//     } else if (human === "paper" && computer.toLowerCase() === "rock") {
-//         return winner = human
-//     } else if (human === "scissors" && computer.toLowerCase() === "paper") {
-//         return winner = human
-//     } else {
-//         return winner = 0;
-//     }
-// }
-
-// checkWinner(computerSelection, humanSelection);
-
-// console.log(winner);
-
-// // // Display round winner message
-// // if (winner = computer) {
-// //     console.log("Sorry, ${computerSelection} beats ${humanSelection}!")
-// // } else if (winner = human) {
-// //     console.log("Congrats! ${humanSelection} beats ${computerSelection}!")
-// // } else {
-// //     console.log("Wow, it was a tie! You both selected ${humanSelection}.")
-// // }
-
-// // Increment the score of the winner
-// humanScore++;
-// computerScore++;
-
-// function playRound(humanChoice, computerChoice) {
-//     console.log(humanChoice);
-//     console.log(computerChoice);
-// }
-
-// playRound(humanSelection, computerSelection);
-
-
-
-
-// // Display the round winner
-// // Increment the score of the winner
+// Display winner
